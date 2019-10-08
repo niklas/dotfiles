d5ce290fd3468ad85c7d3fd0b80f72e82a19e2d9
Niklas Hofer <niklas+dev@lanpartei.de>
2019-10-08 16:57:06 +0200

    use thing function in org tables to calculate with units.

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -331,6 +331,9 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
+  (defmath uconvert (v u)
+    "Convert value V to compatible unit U."
+    (math-convert-units v u))
   )
 
 (defun dotspacemacs/user-config ()
